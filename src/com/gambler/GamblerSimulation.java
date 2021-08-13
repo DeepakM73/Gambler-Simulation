8f548553de0da39c5abd49f552149d024c1ac79d
DeepakM73 <deepak.muddasani73@gmail.com>
2021-08-13 09:24:23 +0530

    Initial Stakes

--- src/com/gambler/GamblerSimulation.java
+++ src/com/gambler/GamblerSimulation.java
@@ -2,7 +2,10 @@ package com.gambler;
 
 public class GamblerSimulation {
 
+    public static int INITIALSTAKE = 100;
+    public static int BET_PER_GAME = 1;
+
     public static void main(String[] args) {
-	System.out.print("Welcome to Gamblers Simulation");
+
     }
 }
